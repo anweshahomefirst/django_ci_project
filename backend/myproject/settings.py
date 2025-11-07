50c44e42098ec61e57772a521b38bad5e5680ea1
Anwesha Chakraborty <anwesha.chakraborty@homefirstindia.com>
2025-11-07 15:37:45 +0530

    feat: Add test comment to trigger CI

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -130,3 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# This is a test comment to trigger CI
